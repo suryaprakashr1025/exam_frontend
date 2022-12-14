0b4b70e2267d53748d4ecb1e9b901eb8e90e1eb5
surya <suryaprakashr1025@gmail.com>
2022-12-14 12:31:02 +0530

    exam_fronend

---
+++ src/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./AdminDashboard.css"
+function AdminDashboard() {
+
+  return (
+    <div>
+      <h1 className='admindashboard' style={{ color:"white"}}>Welcome to AdminDashboard</h1>
+    </div>
+  )
+}
+
+export default AdminDashboard
--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import ChangePassword from './Changepassword';
+import Forgetpassword from './Forgetpassword';
+import AdminDashboard from './AdminDashboard';
+import UserDashboard from './UserDashboard';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/changepassword" element={<ChangePassword/>}/>
+      <Route path="/forgetpassword" element={<Forgetpassword/>}/>
+      <Route path="/admindashboard" element={<AdminDashboard/>}/>
+      <Route path="/userdashboard" element={<UserDashboard/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
